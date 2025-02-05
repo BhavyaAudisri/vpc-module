98f1c70112ef6484f607a53063d67710a1dc260a
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-02-05 23:05:23 +0530

    vpc-mod-test

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "somi-vpc-mod-test"
+    key    = "vpc-test" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+    region = "us-east-1"
+    dynamodb_table = "somi-vpc-mod-test"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
