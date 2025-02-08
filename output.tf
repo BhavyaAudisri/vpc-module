66b159f53ca5fdd1a68aa6949b38497fb0d23669
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-02-08 22:55:46 +0530

    terraform vpc test updated

--- output.tf
+++ output.tf
@@ -2,17 +2,17 @@
 #     value = module.vpc.azs_info
 # }
 output "elastic_ip" {
-    value = module.vpc.elastic_ip
+  value = module.vpc.elastic_ip
 }
 
 output "pub_route_id" {
-    value = module.vpc.pub_route_id
+  value = module.vpc.pub_route_id
 }
 output "pri_route_id" {
-    value = module.vpc.pri_route_id
+  value = module.vpc.pri_route_id
 }
 output "db_route_id" {
-    value = module.vpc.db_route_id
+  value = module.vpc.db_route_id
 }
 # output "default_vpc_id" {
 #     value = module.vpc.default_vpc_id
