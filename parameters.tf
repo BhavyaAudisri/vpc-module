9829076d1518b14501caf8974fa11e983534b901
BhavyaAudisri <somisetty.bhavya@gmail.com>
2025-04-11 18:29:28 +0530

    vpc

--- parameters.tf
+++ parameters.tf
@@ -2,10 +2,11 @@ resource "aws_ssm_parameter" "vpc_id" {
   name  = "/${var.project_name}/${var.environment}/vpc_id"
   type  = "String"
   value = module.vpc.vpc_id
+  overwrite = true
 }
 resource "aws_ssm_parameter" "eip_id" {
   name  = "/${var.project_name}/${var.environment}/eip_id"
   type  = "String"
   value = module.vpc.elastic_ip
-  allow_overwrite = true
+  overwrite = true
 }
